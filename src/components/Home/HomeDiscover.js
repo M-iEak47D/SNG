44733d920da2448b4cece61bc8fa6b8cadc2e5c2
Diwas Paudel <diwasp1@gmail.com>
2021-02-16 14:41:01 +0545

    all

--- src/components/Home/HomeDiscover.js
+++ src/components/Home/HomeDiscover.js
@@ -6,7 +6,7 @@ function HomeDiscover() {
       <div className="discover-locations">
         <div className="container">
           <div className="row">
-            <div className="col-md-6">
+            <div className="col-lg-6">
               <div className="discover-location-content">
                 <div className="best-resort-title">BEST RESORT</div>
                 <p className="discover-location-title">
@@ -15,13 +15,13 @@ function HomeDiscover() {
                   Our Hotel
                 </p>
                 <p className="discover-location-text">
-                  A lovely place to relax and enjoy the serenity of the city and away 
-                  from the crowd of Kathmandu.
+                  A lovely place to relax and enjoy the serenity of the city and
+                  away from the crowd of Kathmandu.
                 </p>
               </div>
             </div>
-            <div className="col-md-6">
-              <div className="discover-location-image">
+            <div className="col-lg-6 ">
+              <div className="discover-location-image d-none d-lg-block">
                 <img
                   src={
                     process.env.PUBLIC_URL +
@@ -35,38 +35,38 @@ function HomeDiscover() {
         <div className="hotel-facilities">
           <div className="container">
             <div className="row">
-              <div className="col-md-4">
+              <div className="col-lg-4">
                 <div className="hotel-facility-item">
                   <img src={process.env.PUBLIC_URL + "/images/bar-icon.JPG"} />
                   <p className="facility-title">Bar and Restaurant</p>
                   <p className="facility-text">
-                     Enjoy different types of liquors and drinks alongside best cuisine
-                     prepared by our chef. 
+                    Enjoy different types of liquors and drinks alongside best
+                    cuisine prepared by our chef.
                   </p>
                 </div>
               </div>
-              <div className="col-md-4">
+              <div className="col-lg-4">
                 <div className="hotel-facility-item">
                   <img
                     src={process.env.PUBLIC_URL + "/images/spa-salon-icon.JPG"}
                   />
                   <p className="facility-title">Parking Space</p>
                   <p className="facility-text">
-                    Plenty of space for your vehicles to park for over night with 
-                    security built in for your ride.
+                    Plenty of space for your vehicles to park for over night
+                    with security built in for your ride.
                   </p>
                 </div>
               </div>
 
-              <div className="col-md-4">
+              <div className="col-lg-4">
                 <div className="hotel-facility-item">
                   <img
                     src={process.env.PUBLIC_URL + "/images/swimming-icon.JPG"}
                   />
                   <p className="facility-title">Meeting Hall</p>
                   <p className="facility-text">
-                   A entire place for meeting hall that can be used for occasions like
-                   office meetings & birthday parties.
+                    A entire place for meeting hall that can be used for
+                    occasions like office meetings & birthday parties.
                   </p>
                 </div>
               </div>
